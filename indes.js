67c317e2b40e4eef7721a67c9e1c5f21e2d99c45
/__Endrigo__ <113954684+Endrigogustavo@users.noreply.github.com>
2022-11-27 18:19:24 -0300

    Add files via upload

--- indes.js
+++ indes.js
@@ -1,10 +1,19 @@
-let map;
-
+// Initialize and add the map
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
   });
 }
 
 window.initMap = initMap;
+
+
